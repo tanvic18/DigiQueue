14865c4f863eeb46be80e29d1c2c98685861bd67
tanvic18 <tanvichaudhari81@gmail.com>
2026-02-20 03:43:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import "leaflet/dist/leaflet.css";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
